f647aabdf4c1581133f5623e7980594a779a8932
Angel Lopez <angel.lopez@thinkresearch.com>
2019-03-09 13:37:35 -0500

    Firebase setup

--- src/components/term.js
+++ src/components/term.js
@@ -1,5 +1,8 @@
 import React from "react"
 import ExtraLinks from "../components/extralinks"
+import PropTypes from 'prop-types';
+import Firebase from "../services/firebase"
+
 
 class Term extends React.Component {
   constructor(props) {
@@ -10,12 +13,29 @@ class Term extends React.Component {
     };
   }
 
+  static contextTypes = {
+    firebase: PropTypes.object,
+  };
+
+  getTerm() {
+    console.log(Firebase.glossary()
+      .then((data) => {
+        console.log('DATA', data);
+        return this.setState({definition: data});
+      })
+      .catch((error) => {
+        console.log('ERROR', error);
+      })
+    );
+  }
+
   render() {
     const { title, definition } = this.state;
     const links = ["link1.com", "link2.com", "link3.com"]
 
     return (
       <div>
+        { this.getTerm() }
         <h1 className="term-title">{ title }</h1>
         <p className="term-definition">{ definition }</p>
         <ExtraLinks links={ links }/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,14 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import Youtube from "../components/youtube"
 import Term from "../components/term"
+import Firebase from "../services/firebase"
 
 const IndexPage = () => (
   <div>
     <Youtube />
     <Term title={"RBI"} definition={"THIS IS WHAT AN RBI IS ABOUT"}/>
+    <div></div>
+
   </div>
 );
 
